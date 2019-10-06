6d3255b78e2e242e64291d9d3f4c260e907810e5
Luis Höfer <luis.hoefer@gmail.com>
2019-10-06 10:51:03 +0200

    feat(router): check login state

--- client/src/router.js
+++ client/src/router.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Login from './components/Login';
+import { TokenService } from './services/storage.service';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -14,7 +15,12 @@ export default new Router({
       name: 'home',
       component: Home,
     },
-    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { public: true, loggedOutOnly: true },
+    },
     {
       path: '/about',
       name: 'about',
@@ -26,3 +32,25 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some(record => record.meta.public);
+  const loggedOutOnly = to.matched.some(record => record.meta.loggedOutOnly);
+  const loggedIn = !!TokenService.getToken();
+
+  if (!isPublic && !loggedIn) {
+    return next({
+      path: '/login',
+      query: { redirect: to.fullPath }, // Store the full path to redirect the user to after login
+    });
+  }
+
+  // Do not allow user to visit login page or register page if they are logged in
+  if (loggedIn && loggedOutOnly) {
+    return next('/');
+  }
+
+  next();
+});
+
+export default router;
