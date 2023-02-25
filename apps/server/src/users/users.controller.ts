47e3bed78e54bfaeeaa4e2a8c6c3541c16378cad
Luis Höfer <luis.hoefer@gmail.com>
2023-02-25 08:48:39 +0100

    feat: return user profile

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
@@ -10,7 +9,6 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
@@ -23,7 +21,8 @@ export class UsersController {
 
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    const user = this.usersService.findOneByEmail(req.user.email);
+    return user;
   }
 
   @Get()
