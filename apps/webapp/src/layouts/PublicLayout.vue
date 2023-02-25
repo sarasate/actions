df5b69e4005f811af0c34c15869f7939615410c9
Luis Höfer <luis.hoefer@gmail.com>
2023-02-26 00:21:25 +0100

    feat: add login and public layout views

---
+++ apps/webapp/src/layouts/PublicLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="container m-auto">
+      <nav class="navbar">
+        <RouterLink to="/" class="btn btn-ghost">Home</RouterLink>
+        <ul class="menu menu-horizontal">
+          <li>
+            <RouterLink to="/login">Login</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ apps/webapp/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container m-auto">
+    <div
+      class="form-control max-w-md rounded-lg m-auto p-4 mt-20 bg-base-300 [&>*]:my-2"
+    >
+      <input type="text" class="input" placeholder="email" />
+      <input type="text" class="input" placeholder="password" />
+      <input type="submit" name="Submit" id="" class="btn" />
+    </div>
+  </div>
+</template>
