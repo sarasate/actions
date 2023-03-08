5e6355d71d91dead453a684e2b43a8bce03e259b
Luis Höfer <luis.hoefer@gmail.com>
2023-03-08 08:31:52 +0100

    feat: setup apollo for vue

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,7 +14,23 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:3020/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 app.use(createPinia());
+app.provide(DefaultApolloClient, apolloClient);
 app.use(router);
 
 app.mount("#app");
