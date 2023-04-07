1830d367824d1f94da66ae381ed01fdec0d80e01
Luis Höfer <luis.hoefer@gmail.com>
2023-04-07 08:45:46 +0200

    chore: version controller

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getVersion(): string {
+    return 'Actions: 0.0.1';
   }
 }
