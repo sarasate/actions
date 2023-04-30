bd0a47a16f0a5ed4af6463b23ddbeedc4a1fcb82
Luis Höfer <luis.hoefer@gmail.com>
2023-04-30 08:44:09 +0200

    feat: action property index

--- apps/server/src/actions/entities/action.entity.ts
+++ apps/server/src/actions/entities/action.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Enum, ManyToOne, Property, Unique } from '@mikro-orm/core';
+import {
+  Entity,
+  Enum,
+  Index,
+  ManyToOne,
+  Property,
+  Unique,
+} from '@mikro-orm/core';
 import { BaseEntity } from '../../common/base-entity.entity';
 import { User } from '../../users/entities/user.entity';
 
@@ -33,7 +40,7 @@ export class Action extends BaseEntity {
   status = ActionStatus.OPEN;
 
   @Property()
-  @Unique()
+  @Index()
   userId: string;
 
   @ManyToOne({ nullable: true })
